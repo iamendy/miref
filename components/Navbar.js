9bb432c1a24bcba13ef68ba38de4896a2c28c63e
Nnamdi <17215830+iamendy@users.noreply.github.com>
2023-01-25 20:06:04 +0100

    added toggle

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,35 @@
 import style from './Navbar.module.css';
 import Link from "next/link";
 import {useRouter} from "next/router";
+import {useState} from "react";
 
 const Navbar = () => {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className={style.navbar}>
+    <nav className="relative">
       <div className={`${style.navbar} inner-width`}>
         <Link className={style.bigMenuItem} passHref href="/">
-          <img src="/img/miref-logo.png" alt="miref logo"/>
+
+            <img src="/img/miref-logo.png" alt="miref logo"/>
+
         </Link>
 
         <div className={style.bigMenuItems}>
-          <Link className={`${style.bigMenuItem} ${ router.asPath === '/' ? 'active' : '' }`} passHref href="/">
+          <Link className={`${style.bigMenuItem} ${ router.asPath === '/' ? 'active' : '' }`}  href="/">
             Home
           </Link>
 
-          <Link className={`${style.bigMenuItem} ${ router.asPath === '/about' ? 'active' : '' }`} passHref href="/about">
+          <Link className={`${style.bigMenuItem} ${ router.asPath === '/about' ? 'active' : '' }`}  href="/about">
             About Us
           </Link>
 
-          <Link className={`${style.bigMenuItem} ${ router.asPath === '/business' ? 'active' : '' }`} passHref href="/business">
+          <Link className={`${style.bigMenuItem} ${ router.asPath === '/business' ? 'active' : '' }`}  href="/business">
             For Business
           </Link>
 
-          <Link className={`${style.bigMenuItem} ${ router.asPath === '/referral' ? 'active' : '' }`} passHref href="/referral">
+          <Link className={`${style.bigMenuItem} ${ router.asPath === '/referral' ? 'active' : '' }`}  href="/referral">
             For Referral
           </Link>
 
@@ -36,16 +40,16 @@ const Navbar = () => {
             Get Started
           </a>
 
-          <img src="img/burger.svg" alt="burger menu" className={style.open}/>
+          <img src="img/burger.svg" alt="burger menu" className={style.open} onClick={()=>setIsOpen(true)}/>
         </div>
 
       </div>
+      {/*{`${style.mobileMenu} show`}*/}
+      <div className={style.mobileMenu} style={{display: isOpen ? 'flex' : ''}}>
 
-      <div className={style.mobileMenu}>
-
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-             className={style.close}>
-          <path stroke-linecap="round" stroke-linejoin="round"
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
+             className={style.close} onClick={()=>setIsOpen(false)}>
+          <path strokeLinecap="round" strokeLinejoin="round"
                 d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
         </svg>
 
