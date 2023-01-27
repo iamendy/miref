315ce96bb7facf93a51dd2ffe4f7c0dd84164dd1
Nnamdi <17215830+iamendy@users.noreply.github.com>
2023-01-27 23:16:04 +0100

    added framer

--- components/GuestLayout.js
+++ components/GuestLayout.js
@@ -1,13 +1,30 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import {motion} from "framer-motion";
+
+const variants = {
+  hidden: { opacity: 0, x: 0, y: 20 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: -0, y: 20 }
+}
 
 const GuestLayout = ({children}) => {
   return (
-    <main>
+    <>
       <Navbar/>
-      {children}
-      <Footer/>
-    </main>
+      <motion.main
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        variants={variants}
+        transition={{ duration: 0.4, type: 'easeInOut' }}
+        style={{ position: 'relative' }}
+      >
+
+        {children}
+        <Footer/>
+      </motion.main>
+    </>
   );
 };
 
